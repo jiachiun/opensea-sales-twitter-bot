351983e5871facd34c2b572ab72784b7616f972b
JayC Ker <jiachiun@gmail.com>
2021-10-14 19:09:10 +0800

    Allow Multiple Twitter Clients

--- tweet.js
+++ tweet.js
@@ -1,41 +1,57 @@
 const axios = require('axios');
 const twit = require('twit');
 
-// Sale Bot
-const twitterConfig = {
+// KIA_Sales_Bot
+const twitterConfig_KIA = {
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN_KEY,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
 };
 
-// KIA Official
-const twitterConfig2 = {
+// KIASalesBot (OFficial)
+const twitterConfig_KIA2 = {
     consumer_key: process.env.CONSUMER_KEY,
     consumer_secret: process.env.CONSUMER_SECRET,
     access_token: process.env.ACCESS_TOKEN_KEY_2,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET_2,
 };
 
-const twitterClient = new twit(twitterConfig);
-const twitterClient2 = new twit(twitterConfig2);
+// CyberHornetsBot
+const twitterConfig_CYBERHORNETS = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN_KEY_CYBERHORNETS,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET_CYBERHORNETS,
+};
+
+// Twitter Clients
+const twitterClient_KIA1 = new twit(twitterConfig_KIA1);
+const twitterClient_KIA2 = new twit(twitterConfig_KIA2);
+const twitterClient_CYBERHORNETS = new twit(twitterConfig_CYBERHORNETS);
+
+function getTwitterClient(twitterClient) {
+    switch(twitterClient) {
+        case "KIA":
+            return twitterClient_KIA;
+        case "KIA2":
+            return twitterClient_KIA2;
+        case "CYBERHORNETS":
+            return twitterClient_CYBERHORNETS;
+        default:
+            return null;
+    }
+}
 
 // Tweet a text-based status
-async function tweet(tweetText) {
+async function tweet(twitterClient, tweetText) {
     const tweet = {
         status: tweetText,
     };
+    
+    const twitterClient = getTwitterClient(twitterClient);
 
     twitterClient.post('statuses/update', tweet, (error, tweet, response) => {
-        if (!error) {
-            console.log(`Successfully tweeted: ${tweetText}`);
-        } else {
-            console.error(error);
-        }
-    });
-    
-    // Client 2
-    twitterClient2.post('statuses/update', tweet, (error, tweet, response) => {
         if (!error) {
             console.log(`Successfully tweeted: ${tweetText}`);
         } else {
@@ -45,10 +61,12 @@ async function tweet(tweetText) {
 }
 
 // OPTIONAL - use this method if you want the tweet to include the full image file of the OpenSea item in the tweet.
-async function tweetWithImage(tweetText, imageUrl) {
+async function tweetWithImage(twitterClient, tweetText, imageUrl) {
     // Format our image to base64
     const processedImage = await getBase64(imageUrl);
 
+    const twitterClient = getTwitterClient(twitterClient);
+    
     // Upload the item's image from OpenSea to Twitter & retrieve a reference to it
     twitterClient.post('media/upload', { media_data: processedImage }, (error, media, response) => {
         if (!error) {
@@ -58,26 +76,6 @@ async function tweetWithImage(tweetText, imageUrl) {
             };
 
             twitterClient.post('statuses/update', tweet, (error, tweet, response) => {
-                if (!error) {
-                    console.log(`Successfully tweeted: ${tweetText}`);
-                } else {
-                    console.error(error);
-                }
-            });
-        } else {
-            console.error(error);
-        }
-    });
-    
-    // Twitter Client 2
-    twitterClient2.post('media/upload', { media_data: processedImage }, (error, media, response) => {
-        if (!error) {
-            const tweet = {
-                status: tweetText,
-                media_ids: [media.media_id_string]
-            };
-
-            twitterClient2.post('statuses/update', tweet, (error, tweet, response) => {
                 if (!error) {
                     console.log(`Successfully tweeted: ${tweetText}`);
                 } else {
