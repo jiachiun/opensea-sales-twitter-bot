80ec52dbb2214af7e975543d53993a01e8fafbc8
Jia Chiun <jiachiun@gmail.com>
2021-11-18 12:18:39 +0800

    Update

--- app.js
+++ app.js
@@ -72,8 +72,8 @@ function showLinks(message) {
             { name: 'Social Media', value: '`<https://twitter.com/KoalaAgencyNFT>\n<https://tiktok.com/@koalaagencynft>\n<https://instagram.com/koala.agency.nft>\n`' },
             { name: 'Verified Contract', value: '`<https://etherscan.io/address/0x3f5fb35468e9834a43dca1c160c69eaae78b6360>\n`' },
             { name: 'Rarity Tools:', value: '`<https://rarity.tools/koala-intelligence-agency>\n`' },
-            { name: 'HackMD  (Roadmap V2)', value: '<https://hackmd.io/@sJX7GMieToGIMmb_nnCnFQ/kia-v2>\n' },
-            { name: '\n', value: 'Links can be found in #official-kia-links' },
+            { name: 'HackMD  (Roadmap V2)', value: `'<https://hackmd.io/@sJX7GMieToGIMmb_nnCnFQ/kia-v2>\n'` },
+            { name: '\u200B', value: 'Links can be found in #official-kia-links' },
         )
 
     message.channel.send(msg);
