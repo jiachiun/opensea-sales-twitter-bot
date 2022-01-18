7e76afd493cb56c5ad4ec84b0114550c4b9cea40
JayC Ker <jayc@jayc.tech>
2022-01-18 08:07:07 +0800

    Add a second API Key

--- app.js
+++ app.js
@@ -667,7 +667,7 @@ setInterval(() => {
 
     axios.get('https://api.opensea.io/api/v1/events', {
         headers: {
-            "X-API-KEY": process.env.OPENSEA_API_KEY,
+            "X-API-KEY": process.env.OPENSEA_API_KEY_JC,
         },
         params: {
             collection_slug: "roo-troop",
@@ -711,7 +711,7 @@ setInterval(() => {
 
     axios.get('https://api.opensea.io/api/v1/events', {
         headers: {
-            "X-API-KEY": process.env.OPENSEA_API_KEY,
+            "X-API-KEY": process.env.OPENSEA_API_KEY_JC,
         },
         params: {
             collection_slug: "roo-troop",
